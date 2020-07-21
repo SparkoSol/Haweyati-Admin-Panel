eb96376acd8012434f39527c14a9b3e5579352d5
Nabeel Ahmed <nabeelrajpoot314@gmail.com>
2020-07-21 13:50:26 +0500

    supplier dumpster images to image

--- pages/store/Construction-Dumpster/_id.vue
+++ pages/store/Construction-Dumpster/_id.vue
@@ -18,7 +18,7 @@ export default {
   },
   data: () => ({
     columns: [
-      { text: 'Image', value: 'images', width: '90px' },
+      { text: 'Image', value: 'image', width: '90px' },
       { text: 'Size', value: 'size' },
       { text: 'Description', value: 'description' }
     ]
